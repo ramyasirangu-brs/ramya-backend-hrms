6f76b90a45fd7d4d3dd1bb74291b08f6a75e2983
ramya <ramyasirangu@gmail.com>
2025-08-06 12:51:18 +0530

    add updated backend files

--- src/main/java/com/vagarious/backend/hrms/model/PersonalDetails.java
+++ src/main/java/com/vagarious/backend/hrms/model/PersonalDetails.java
@@ -1,8 +1,10 @@
 package com.vagarious.backend.hrms.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
 
 @Embeddable
+@Data
 public class PersonalDetails {
     private String dob;
     private String gender;
